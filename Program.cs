a0590f5e6384d920774af7ff6e8b5230a3f94e37
GitNub-Community-1 <maxik010020003@gmail.com>
2025-08-12 15:59:04 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿//Task1
+// void Univ(int a)
+// {
+//     int evencount = 0;
+//     int oddcount = 0;
+//     int zerocount = 0;
+//     int digitcount = 0;
+//     int sumdigit = 0;
+//     int cnt = 0;
+//     int mn = 999999;
+//     int mx = -99999;
+//     while (a != 0)
+//     {
+//         cnt = a % 10;
+//         if (cnt % 2 == 0)
+//         {
+//             evencount++;
+//         }
+//         if (cnt % 2 != 0)
+//         {
+//             oddcount++;
+//         }
+//         if (cnt == 0)
+//         {
+//             zerocount++;
+//         }
+//         digitcount++;
+//         if (cnt < mn)
+//         {
+//             mn = cnt;
+//         }
+//         if (cnt > mx)
+//         {
+//             mx = cnt;
+//         }
+//         sumdigit += cnt;
+//         a = a / 10;
+//     }
+//     System.Console.WriteLine($"Even: {evencount}");
+//     System.Console.WriteLine($"Odd: {oddcount}");
+//     System.Console.WriteLine($"Zeros: {zerocount}");
+//     System.Console.WriteLine($"Digits: {digitcount}");
+//     System.Console.WriteLine($"Min: {mn}");
+//     System.Console.WriteLine($"Max: {mx}");
+//     System.Console.WriteLine($"Sum of Digits: {sumdigit}");
+// }
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// Univ(a);
+
+//Task2
+// void Grade(int a)
+// {
+//     if (a >= 90 && a <= 100)
+//     {
+//         System.Console.WriteLine("A");
+//     }
+//     if (a >= 80 && a <= 89)
+//     {
+//         System.Console.WriteLine("B");
+//     }
+//     if (a >= 70 && a <= 79)
+//     {
+//         System.Console.WriteLine("C");
+//     }
+//     if (a >= 60 && a <= 69)
+//     {
+//         System.Console.WriteLine("D");
+//     }
+//     if (a >= 0 && a <= 59)
+//     {
+//         System.Console.WriteLine("F");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Sorry? But Grade Ball Not Added In International School, Please Past The Grades in Second Categories: ");
+//         System.Console.WriteLine("Grade A: 90-100");
+//         System.Console.WriteLine("Grade B: 80-89");
+//         System.Console.WriteLine("Grade C: 70-79");
+//         System.Console.WriteLine("Grade D: 60-69");
+//         System.Console.WriteLine("Grade F: 0-59");
+//     }
+// }
+// int a = Convert.ToInt32(Console.ReadLine());
+// Grade(a);
